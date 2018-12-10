c208317979b3bee2e4035e24046e4c546488143e
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-12-10 08:07:40 +0600

    Implement resolver function for BookType using dummy data.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// Dummy data
+const books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -21,6 +29,7 @@ const RootQuery = new GraphQLObjectType({
             },
             resolve(parent, args) {
                 // code to get data from DB/other source
+                return _.find(books, { id: args.id });
             }
         }
     }
