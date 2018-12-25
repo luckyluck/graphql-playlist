8ae2d70e1717fbcfb6923da433fbccf526edc072
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-12-25 20:41:31 +0600

    Add MongoDB connection using mongoose.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema/schema');
+const keys = require('./config/keys');
 
 const app = express();
+
+// connect to mLab database
+// make sure to replace db creds
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
