df20143b6d3a7d8fcd1ac37200967edb308c0ec4
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-12-27 18:46:29 +0600

    Making queries from React amd rendering data in a list component. Add cors middleware for our Express server.

--- server/app.js
+++ server/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const schema = require('./schema/schema');
 const keys = require('./config/keys');
 
 const app = express();
 
-// connect to mLab database
-// make sure to replace db creds
+// Allow cross-origin requests
+app.use(cors());
+
+// Connect to mLab database
+// Make sure to replace db creds
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     console.log('connected to database');
